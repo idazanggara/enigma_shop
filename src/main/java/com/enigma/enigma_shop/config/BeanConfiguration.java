78cc47068dfac4cf12ad13f944417401d79801c0
idazanggara <idazideas@gmail.com>
2024-05-27 14:52:22 +0700

    Rest-Client Payment awal

--- src/main/java/com/enigma/enigma_shop/config/BeanConfiguration.java
+++ src/main/java/com/enigma/enigma_shop/config/BeanConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class BeanConfiguration {
@@ -22,5 +23,10 @@ public class BeanConfiguration {
 	// tapi nanti ada exeption, enggak usah di try-cath tapi di throws aja method authenticationManager
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
 		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public RestClient restClient() {
+		return RestClient.create();
 	}
 }
